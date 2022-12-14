9bf0da611d7606aa7a1de6135b0b3fd4d2c15547
Valéria <valeriamenezes022@gmail.com>
2022-12-14 00:49:08 -0300

    criando rotas de endpoint e fazendo validações de cadastro por meio de middlewares

---
+++ scr/middlewares/validateDescription.js
@@ -0,0 +1,40 @@
+// const validateDescription = (req, res, next) => {
+//   const { description } = req.body;
+//   if (!description) {
+//     return res.status(400).json({ message: 'O campo description é obrigatório' });
+//     }
+//     if (!description.createdAt) {
+//       return res.status(400).json({ message: 'O campo createdAt é obrigatório' });
+//     }
+//     if (!description.rating) {
+//       return res.status(400).json({ message: 'O campo rating é obrigatório' });
+//     }
+//     if (!description.difficulty) {
+//       return res.status(400).json({ message: 'O campo difficulty é obrigatório' });
+//     }
+
+//     next();
+// };
+
+const validateCases = (descriptionValue, res, value) => {
+  if (!descriptionValue) {
+    return res.status(400).json(
+      { message: `O campo ${value} é obrigatório` },
+    );
+  }
+};
+
+const validateDescription = (req, res, next) => {
+  const { description } = req.body;
+
+  return validateDescription(description, res, 'description')
+    || validateDescription(description.createdAt, res, 'createdAt')
+    || validateDescription(description.rating, res, 'rating')
+    || validateDescription(description.difficulty, res, 'difficulty')
+    || next();
+};
+
+module.exports = {
+  validateDescription,
+  validateCases,
+};
